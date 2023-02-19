ff31d0328c45c3c1a468d4ae96e961ec44d39e52
Mubeen Hameed <rabimubeen955@gmail.com>
2023-02-20 04:05:46 +0500

    Update petrolium.cpp

--- petrolium.cpp
+++ petrolium.cpp
@@ -1,6 +1,3 @@
-/*The price of petroleum in Pakistan is Rs. 238.56 per litre. Write a program to take the
-number of litres as an input from the user (fuel dispenser) and compute the total bill to be
-charged to the customer.*/
 
 #include<iostream>
 using namespace std;
